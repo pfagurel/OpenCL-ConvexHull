f96760b8df57f86513b4129de943613f60355fbe
pfagurel <piter.fagurel@mail.ru>
2021-11-30 11:28:23 +0100

    update + doc

--- Exports/Windows/data/points_generator.py
+++ Exports/Windows/data/points_generator.py
@@ -2,16 +2,14 @@ import numpy as np
 import sys
 
 if __name__ == "__main__":
-    random_vec1 = np.random.randint(int(sys.argv[1]), size=(int(sys.argv[1])),dtype=np.int64)
-    random_vec2 = np.random.randint(int(sys.argv[1]), size=(int(sys.argv[1])),dtype=np.int64)
-    unique_random_vec1 = np.unique(random_vec1)
-    unique_random_vec2 = np.unique(random_vec2)
-    np.random.shuffle(unique_random_vec1)
-    np.random.shuffle(unique_random_vec2)
-    m_len = min(len(unique_random_vec1),len(unique_random_vec2))
+    random_vec1 = np.arange(int(sys.argv[1]))
+    random_vec2 = np.arange(int(sys.argv[1]))
+    np.random.shuffle(random_vec1)
+    np.random.shuffle(random_vec2)
+    m_len = len(random_vec1)
         
     
     f = open(sys.argv[1]+".txt", "w")
     for i in range(m_len):
-        f.write(str(unique_random_vec1[i]) + " " + str(unique_random_vec2[i]) + '\n')
+        f.write(str(random_vec1[i]) + " " + str(random_vec2[i]) + '\n')
     f.close()
